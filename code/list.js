77408444d409160c9d7a0e9d55ef2791b93b3d68
Glynn Bird <glynn.bird@gmail.com>
2023-09-22 16:02:01 +0100

    missed one file

--- code/list.js
+++ code/list.js
@@ -1,24 +1,16 @@
 import { okResponse } from './lib/constants.js'
 import { mustBePOST, mustBeJSON, apiKey } from './lib/checks.js'
-
+import { list } from './lib/db.js'
+ 
 export async function onRequest(context) {
   // handle POST/JSON/apikey chcecks
   const r = apiKey(context.request, context.env) || mustBePOST(context.request) || mustBeJSON(context.request)
   if (r) return r
 
   // list keys in the KV store, bound to this worker as TVKV
-  const l = await context.env.TVKV.list()
-
-  // map to a list of objects
-  const output = l.keys.map((k) => {
-    // k.name = '1681480420026'
-    return {
-      id: k.name,
-      ...k.metadata
-    }
-  })
+  const response = await list(context.env.TVKV)
 
   // send response
-  return new Response(JSON.stringify({ ok: true, list: output }), okResponse)
+  return new Response(JSON.stringify({ ok: true, list: response }), okResponse)
 
 }

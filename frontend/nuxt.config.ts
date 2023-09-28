fdb848457aad6168cf5a68e0569e515ba7692789
Glynn Bird <glynn.bird@gmail.com>
2023-09-28 13:33:33 +0100

    pwa?

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,5 +1,32 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ['@vite-pwa/nuxt'],
+  pwa: {
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'TV Helper',
+      short_name: 'TV',
+      theme_color: '#ffffff',
+      icons: [
+        {
+          src: 'tv_192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: 'tv_512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+        {
+          src: 'tv_512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any maskable',
+        }
+      ]
+    }
+  },
   ssr: false,
   runtimeConfig: {
     public: {

ca827b98af00fa1b5f627807205882b3271a4dcc
Glynn Bird <glynn.bird@gmail.com>
2023-10-03 10:38:58 +0100

    show future items

--- frontend/components/ProgList.vue
+++ frontend/components/ProgList.vue
@@ -2,14 +2,19 @@
   // input attributes
   const { progs } = defineProps(['progs'])
 
-  // emitted events
-  //defineEmits(['deleteItem'])
+  // local page items
+  const now = ref(0)
+  now.value = new Date().toISOString()
+
 </script>
 <template>
   <v-list>
     <v-list-item v-for="prog in progs" :key="prog.id" :to="`/prog/${prog.id}`">
       <v-list-item-title>{{  prog.title }}</v-list-item-title>
-      <v-list-item-subtitle>{{ prog.date }} <v-chip label size="x-small" color="primary" v-if="prog.watching">Watching</v-chip></v-list-item-subtitle>
+      <v-list-item-subtitle>{{ prog.date }}
+        <v-chip label size="x-small" color="primary" v-if="prog.watching">Watching</v-chip>
+        <v-chip label size="x-small" color="secondary" v-if="prog.date>now">Future</v-chip>
+      </v-list-item-subtitle>
       <template v-slot:append>
         <v-btn
           color="grey-lighten-1"

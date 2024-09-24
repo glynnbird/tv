cbfe05b9b0370bb04a2b80d4270c1d6f35a1b3ef
Glynn Bird <glynn.bird@gmail.com>
2024-09-24 10:55:57 +0100

    nuxt update

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -2,21 +2,27 @@
 export default defineNuxtConfig({
   modules: [],
   ssr: false,
+
   runtimeConfig: {
     public: {
       apiBase: 'https://tv.glynnbird.com'
     }
   },
+
   css: [
     'vuetify/lib/styles/main.sass',
     '@mdi/font/css/materialdesignicons.min.css'
   ],
+
   build: {
     transpile: ['vuetify'],
   },
+
   vite: {
     define: {
       'process.env.DEBUG': false,
     }
-  }
+  },
+
+  compatibilityDate: '2024-09-24'
 })

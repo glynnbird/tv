7963c89901c4e5729b6316944285ef2c093e5a41
Glynn Bird <glynn.bird@gmail.com>
2024-12-02 16:47:56 +0000

    progress

--- frontend/components/ProgList.vue
+++ frontend/components/ProgList.vue
@@ -13,13 +13,19 @@
   const channels = ref(2)
   channels.value = ['BBC','ITV','Channel4','Channel5','SkyAtlantic','Alba']
 </script>
+<style>
+.sep {
+  margin-left:5px;
+  margin-right:5px;
+}
+</style>
 <template>
   <v-list>
     <v-list-item v-for="prog in progs" :key="prog.id" :to="`/prog/${prog.id}`">
       <v-list-item-title>{{  prog.title }} <v-chip v-if="prog.on" label size="x-small">{{ prog.on }}</v-chip></v-list-item-title>
       <v-list-item-subtitle>{{ prog.date }}
         <v-icon size="x-small" color="primary" v-if="prog.watching">mdi-television-play</v-icon>
-        <v-chip color="primary" v-if="prog.uptoep && prog.uptomax">{{ prog.uptoep }} / {{ prog.uptomax }}</v-chip>
+        <v-chip class="sep" color="primary" label size="x-small" v-if="prog.uptoep && prog.uptomax">{{ prog.uptoep }} / {{ prog.uptomax }}</v-chip>
         <v-icon size="x-small" color="secondary" v-if="prog.date>now">mdi-calendar-clock</v-icon>
         <v-icon size="x-small" color="warning" v-if="prog.date > now && prog.date < nextWeek">mdi-calendar-week</v-icon>
         <v-icon size="x-small" color="red" v-if="prog.date > now && prog.date < nextWeek && channels.includes(prog.on)">mdi-record</v-icon>

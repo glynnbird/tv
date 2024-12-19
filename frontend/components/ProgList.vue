3421491d60c6e78581819e4a2ce552c338f86552
Glynn Bird <glynn.bird@gmail.com>
2024-12-19 10:58:46 +0000

    pics

--- frontend/components/ProgList.vue
+++ frontend/components/ProgList.vue
@@ -17,28 +17,27 @@
 .sep {
   margin-right:5px;
 }
+.cardsep {
+  margin-bottom:20px; 
+}
 </style>
 <template>
-  <v-list>
-    <v-list-subheader style="user-select:none;">{{ heading }}</v-list-subheader>
-    <v-list-item v-for="prog in progs" :key="prog.id" :to="`/prog/${prog.id}`">
-      <v-list-item-title>{{  prog.title }}</v-list-item-title>
-      <v-list-item-subtitle>
+  <h4>{{ heading }}</h4>
+  <v-card class="cardsep" v-for="prog in progs" :key="prog.id" :to="`/prog/${prog.id}`">
+    <v-img height="300" cover :src="`https://tv.glynnbird.com/api/img?id=${prog.id}`" gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)">
+      <v-card-title class="text-white">
+        {{  prog.title }}
         <v-chip class="sep" v-if="prog.on" label size="x-small">{{ prog.on }}</v-chip>
-        <v-chip class="sep" color="primary" label size="x-small" v-if="prog.uptoep && prog.uptomax">{{ prog.uptoep }} / {{ prog.uptomax }}</v-chip>
-        <v-chip class="sep" color="primary" label size="x-small" v-if="prog.type==='Film' || prog.type=='Single'">{{ prog.type }}</v-chip>
-        <v-icon class="sep" size="x-small" color="red" v-if="prog.date > now && prog.date < nextWeek && channels.includes(prog.on)">mdi-record</v-icon>
+        <v-chip class="sep" label size="x-small" v-if="prog.uptoep && prog.uptomax">{{ prog.uptoep }} / {{ prog.uptomax }}</v-chip>
+        <v-chip class="sep" label size="x-small" v-if="prog.type==='Film' || prog.type=='Single'">{{ prog.type }}</v-chip>
+      </v-card-title>
+      <v-card-subtitle>
         <v-chip class="sep" variant="flat" size="x-small" color="secondary" v-if="prog.date > now">{{ prog.date }}</v-chip>
-      </v-list-item-subtitle>
-      <template v-slot:append>
-        <v-btn
-          color="grey-lighten-1"
-          icon="mdi-chevron-right"
-          variant="text"
-        ></v-btn>
-      </template>
-    </v-list-item>
-  </v-list>
+        <v-icon class="sep" size="x-small" color="red" v-if="prog.date > now && prog.date < nextWeek && channels.includes(prog.on)">mdi-record</v-icon>
+      </v-card-subtitle>
+    </v-img>
+  </v-card>
+
   <!-- if empty, show instructions -->
   <div v-if="progs.length === 0">
     Your list is empty. Start by clicking the <NuxtLink href="/add">+</NuxtLink> icon in the top left.

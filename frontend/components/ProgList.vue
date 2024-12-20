cd22da21ace1bfd9388c64010aad2fdbc861d6ad
Glynn Bird <glynn.bird@gmail.com>
2024-12-20 08:35:08 +0000

    larger chips

--- frontend/components/ProgList.vue
+++ frontend/components/ProgList.vue
@@ -50,12 +50,12 @@
     <v-img height="300" cover :src="`https://tv.glynnbird.com/api/img?id=${prog.id}`">
       <v-card-title class="text-white shadow">{{  prog.title }}</v-card-title>
       <v-card-subtitle class="position-absolute bottom-0 left-0 offbot">
-        <v-chip class="sep" v-if="prog.on" variant="flat" color="grey-lighten-2" label size="small">{{ prog.on }}</v-chip>
-        <v-chip class="sep" variant="flat" label size="small" color="grey-lighten-2" v-if="prog.uptoep && prog.uptomax">{{ prog.uptoep }} / {{ prog.uptomax }}</v-chip>
-        <v-chip class="sep" variant="flat" label size="small" color="grey-lighten-2" v-if="prog.type==='Film' || prog.type=='Single'">{{ prog.type }}</v-chip>
-        <v-chip class="sep" variant="flat" label size="small" color="white" v-if="prog.date > now">
+        <v-chip class="sep" v-if="prog.on" variant="flat" color="grey-lighten-2" label size="default">{{ prog.on }}</v-chip>
+        <v-chip class="sep" variant="flat" label size="default" color="grey-lighten-2" v-if="prog.uptoep && prog.uptomax">{{ prog.uptoep }} / {{ prog.uptomax }}</v-chip>
+        <v-chip class="sep" variant="flat" label size="default" color="grey-lighten-2" v-if="prog.type==='Film' || prog.type=='Single'">{{ prog.type }}</v-chip>
+        <v-chip class="sep" variant="flat" label size="default" color="white" v-if="prog.date > now">
           {{ formatDate(prog.date) }}
-          <v-icon class="sep" size="small" color="red" v-if="prog.date > now && prog.date < nextWeek && channels.includes(prog.on)">mdi-record</v-icon>
+          <v-icon class="sep" size="default" color="red" v-if="prog.date > now && prog.date < nextWeek && channels.includes(prog.on)">mdi-record</v-icon>
         </v-chip>
       </v-card-subtitle>
     </v-img>

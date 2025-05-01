e70438d15f7cdec0ebae7f352e2fa631cc0393ec
Glynn Bird <glynn.bird@gmail.com>
2025-05-01 15:25:14 +0100

    bug fix

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -35,6 +35,8 @@
 
   // sort programmes most recently updated first
   function mostRecentlyUpdatedFirst(a, b) {
+    a.ts = a.ts || 0
+    b.ts = b.ts || 0
     if (a.ts < b.ts) {
       return 1;
     } else if (a.ts > b.ts) {

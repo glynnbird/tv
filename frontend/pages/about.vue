de869448e721283b52a438fd5997ed56e898403f
Glynn Bird <glynn.bird@gmail.com>
2025-06-11 14:58:52 +0100

    more details on about page

--- frontend/pages/about.vue
+++ frontend/pages/about.vue
@@ -3,4 +3,10 @@
   <div>
     A simple TV programme list built using the Cloudflare cloud.
   </div>
+  <v-list>
+    <v-list-item>Stores information about TV programmes in a Cloudflare KV database.</v-list-item>
+    <v-list-item>Breaks the list into three parts: Ready to watch, Watching & Future.</v-list-item>
+    <v-list-item>Optional AI helper assists with filling the "add" form by reading web pages about the programme for you.</v-list-item>
+    <v-list-item>Deployed by Terraform and uses Cloudflare Pages to distribute the static website on Cloudflare's CDN.</v-list-item>
+  </v-list>
 </template>

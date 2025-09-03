61ea1a4257e16c5e848c58b44e1c689486aa71c5
Glynn Bird <glynn.bird@gmail.com>
2025-09-03 09:59:56 +0100

    rolldown

--- code/build.sh
+++ code/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-npx rollup -p @rollup/plugin-terser --format=es --file=../functions/api/add.js -- add.js
-npx rollup -p @rollup/plugin-terser --format=es --file=../functions/api/del.js -- del.js
-npx rollup -p @rollup/plugin-terser --format=es --file=../functions/api/get.js -- get.js
-npx rollup -p @rollup/plugin-terser --format=es --file=../functions/api/list.js -- list.js
-npx rollup -p @rollup/plugin-terser --format=es --file=../functions/api/toggle.js -- toggle.js
-npx rollup -p @rollup/plugin-terser --format=es --file=../functions/api/img.js -- img.js
-npx rollup -p @rollup/plugin-terser --format=es --file=../functions/api/ai.js -- ai.js
-npx rollup -p @rollup/plugin-terser --format=es --file=../functions/api/archivelist.js -- archivelist.js
+npx rolldown --format=es --file=../functions/api/add.js -- add.js
+npx rolldown --format=es --file=../functions/api/del.js -- del.js
+npx rolldown --format=es --file=../functions/api/get.js -- get.js
+npx rolldown --format=es --file=../functions/api/list.js -- list.js
+npx rolldown --format=es --file=../functions/api/toggle.js -- toggle.js
+npx rolldown --format=es --file=../functions/api/img.js -- img.js
+npx rolldown --format=es --file=../functions/api/ai.js -- ai.js
+npx rolldown --format=es --file=../functions/api/archivelist.js -- archivelist.js
 

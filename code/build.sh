21bc7802223c9beccd6b926c0e86ea75098c7752
Glynn Bird <glynn.bird@gmail.com>
2026-08-19 09:04:05 +0100

    latest rolldown

--- code/build.sh
+++ code/build.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-npx rolldown --format=es --file=../functions/api/add.js -- add.js
-npx rolldown --format=es --file=../functions/api/del.js -- del.js
-npx rolldown --format=es --file=../functions/api/get.js -- get.js
-npx rolldown --format=es --file=../functions/api/list.js -- list.js
-npx rolldown --format=es --file=../functions/api/toggle.js -- toggle.js
-npx rolldown --format=es --file=../functions/api/img.js -- img.js
-npx rolldown --format=es --file=../functions/api/ai.js -- ai.js
-npx rolldown --format=es --file=../functions/api/archivelist.js -- archivelist.js
+npx rolldown -c rolldown.config.js
 
---
+++ code/rolldown.config.js
@@ -0,0 +1,36 @@
+import { defineConfig } from 'rolldown';
+
+export default defineConfig([
+  {
+    input: 'add.js',
+    output: { format: 'esm', file: '../functions/api/add.js' },
+  },
+  {
+    input: 'ai.js',
+    output: { format: 'esm', file: '../functions/api/ai.js' },
+  },
+    {
+    input: 'archivelist.js',
+    output: { format: 'esm', file: '../functions/api/archivelist.js' },
+  },
+  {
+    input: 'del.js',
+    output: { format: 'esm', file: '../functions/api/del.js' },
+  },  
+  {
+    input: 'get.js',
+    output: { format: 'esm', file: '../functions/api/get.js' },
+  },
+    {
+    input: 'img.js',
+    output: { format: 'esm', file: '../functions/api/img.js' },
+  },
+  {
+    input: 'list.js',
+    output: { format: 'esm', file: '../functions/api/list.js' },
+  },
+    {
+    input: 'toggle.js',
+    output: { format: 'esm', file: '../functions/api/toggle.js' },
+  }
+]);
